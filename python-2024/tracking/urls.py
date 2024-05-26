f9c41afff25899c74b9d8a1a81f2dd0d930eb2e9
tarik561 <tarikyacyk2@gmail.com>
2024-05-26 19:13:58 +0200

    Create account pages

--- python-2024/tracking/urls.py
+++ python-2024/tracking/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import HomepageView
+from .views import HomepageView, LoginView, RegisterView, logout_view
 
 urlpatterns = [
-    path('', login_required(HomepageView.as_view())),
+    path('', login_required(HomepageView.as_view()), name='homepage'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/logout/', logout_view, name='logout'),
 ]
--- python-2024/tracking/views.py
+++ python-2024/tracking/views.py
@@ -1,6 +1,61 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.base import ContextMixin
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 class HomepageView(TemplateView):
   template_name = 'tracking/homepage.html'
+
+
+class RegisterView(TemplateView, ContextMixin):
+  template_name = 'tracking/register.html'
+  extra_context = {}
+
+  def post(self, request):
+    get = request.POST.get
+    try:
+      user = User.objects.create_user(
+        username=get('username'),
+        email=get('email'),
+        password=get('password')
+      )
+      login(request, user)
+      return redirect('/')
+
+    except Exception as error:
+      self.extra_context.update({
+        'error': error,
+      })
+      return super().get(request)
+      
+
+class LoginView(TemplateView, ContextMixin):
+  template_name = 'tracking/login.html'
+  extra_context = {}
+
+  def post(self, request):
+    get = request.POST.get
+    try:
+      user = authenticate(
+        request=request,
+        username=get('username'),
+        password=get('password'),
+      )
+      if user is not None:
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('/')
+      else:
+        self.extra_context.update({'error':'The credentials passed for no user.'})
+        return super().get(request)
+
+    except Exception as error:
+      self.extra_context.update({'error':error})
+      return super().get(request)
+
+
+def logout_view(request):
+  logout(request)
+  return redirect('/accounts/login/')
