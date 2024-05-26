a9ab1adba291330f6c58d7092f83e4ee89cf8b37
tarik561 <tarikyacyk2@gmail.com>
2024-05-26 18:06:37 +0200

    Accidental paste removal

--- python-2024/tracking/admin.py
+++ python-2024/tracking/admin.py
@@ -1,18 +1,6 @@
 from django.contrib import admin
 from .models import UserSession
 
-# Register your models here.
-class UserSession(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    session_key = models.CharField(max_length=40, unique=True)
-    login_time = models.DateTimeField(auto_now_add=True)
-    last_activity = models.DateTimeField(auto_now=True)
-    logout_time = models.DateTimeField(null=True, blank=True)
-
-    def __str__(self):
-        return f'{self.user.username} - {self.session_key}'
-
-
 class UserSessionAdmin(admin.ModelAdmin):
     list_display = ('user', 'session_key', 'login_time', 'last_activity', 'logout_time')
     list_filter = ('user', 'login_time', 'last_activity', 'logout_time')
--- python-2024/tracking/models.py
+++ python-2024/tracking/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserSession(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    session_key = models.CharField(max_length=40)
+    session_key = models.CharField(max_length=40, unique=True)
     login_time = models.DateTimeField(auto_now_add=True)
     last_activity = models.DateTimeField(auto_now=True)
     logout_time = models.DateTimeField(null=True, blank=True)
